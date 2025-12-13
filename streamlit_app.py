74c023fe88e074a96ef524150dd77f4feb4ed822
Krishna Kalluri <krishnasaikalluri@gmail.com>
2025-12-13 13:19:14 -0500

    Integrate Smoothiefroot API for watermelon test data
    Added functionality to fetch and display watermelon data from Smoothiefroot API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie Order :cup_with_straw:")
@@ -34,3 +36,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
